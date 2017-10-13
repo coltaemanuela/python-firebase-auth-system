0fece31b5d4d887d877ba14009fc502152a76654
Emma <emanuela.colta@mobiversal.com>
2017-10-13 12:59:28 +0100

    firebase configuration

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
+
 from flask import Flask
+from flask import current_app
+import pyrebase
+
+config = {  
+	"apiKey": "yourApiKey",
+	"authDomain":"yourprojectId.firebaseapp.com",
+	"databaseURL":"https://yourprojectId.firebaseio.com",
+	"storageBucket":   "yourstorageBucket"
+} 
+
+firebase = pyrebase.initialize_app(config)
+# Get a reference to the database service
+db = firebase.database()
+data = {
+    "name": "emma",
+	"email":"coltaemanuela@gmail.com"
+}
+#db.child("users").push(data)
+users = db.child("users").get()
+print(users.val())
+
+
 app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
 	app.run()
