cb51c7b8d368f0ed03a89f2c84b1879fa9337389
Emma <emanuela.colta@mobiversal.com>
2017-10-31 18:59:24 +0000

    registration and authentication system with Firebase

--- app.py
+++ app.py
@@ -1,34 +1,64 @@
-
 from flask import Flask
 from flask import current_app
 from flask import render_template
 import pyrebase
+from firebase_token_generator import create_token
+import os
+import json
+from flask_restful import Resource, Api
+from flask import request
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'views')
+app = Flask('myapp', template_folder = tmpl_dir)
 
 config = {  
 	"apiKey": "yourApiKey",
 	"authDomain":"yourprojectId.firebaseapp.com",
 	"databaseURL":"https://yourprojectId.firebaseio.com",
-	"storageBucket":   "yourstorageBucket"
+	"storageBucket":   "yourstorageBucket",
+	"serviceAccount":"./yourprojectId.json"
 } 
 
 firebase = pyrebase.initialize_app(config)
-# Get a reference to the database service
-db = firebase.database()
-data = {
-    "name": "emma",
-	"email":"coltaemanuela@gmail.com"
-}
-#db.child("users").push(data)
-users = db.child("users").get()
-print(users.val())
-
-
-app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # return 'Hello, World!'
-	 return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def sign_in():
+	email = request.form['email']
+	password = request.form['password']
+	auth = firebase.auth()
+	user = auth.sign_in_with_email_and_password(email, password)
+	user = auth.refresh(user['refreshToken'])
+	return json.dumps (user)	
+
+@app.route('/signup', methods=['POST'])
+def sign_up():
+	db = firebase.database()
+	auth = firebase.auth()
+
+	email = request.form['email']
+	password = request.form['password']
+	confirm_password = request.form['confirm_password']
+
+	user_details = {
+		"email": request.form['email'],		
+		"telephone": request.form['telephone'],
+		"username": request.form['username'],
+		"street": request.form['street']
+		"city": request.form['city'],
+		"postal_code": request.form['postal_code']
+	}
+
+	if password == confirm_password:
+		db.child("users").push(user_details)
+		auth.create_user_with_email_and_password(email, password)
+		user = auth.sign_in_with_email_and_password(email, password)
+		auth.send_email_verification(user['idToken'])
+
+	return json.dumps(user_details)
 
 if __name__ == '__main__':
 	app.run()
